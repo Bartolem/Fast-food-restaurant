5c93a0fa2331c236a3393f198f4a1eeca9116f7c
Bartosz <bertolem557@gmail.com>
2024-01-24 22:57:46 +0100

    add CustomerCreationTest

--- src/test/java/org/fast_food/customer/CustomerCreationTest.java
+++ src/test/java/org/fast_food/customer/CustomerCreationTest.java
@@ -7,6 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class CustomerCreationTest {
 
     @Test
-    void createCustomerAccount() {
+    void testCreateCustomerAccount() {
+        Customer customer = CustomerCreation.createCustomerAccount("Paulina", "paola5@gmial.com", "345 511 942");
+        assertTrue(CustomerManagement.getCustomers().containsKey(customer.getId()));
+        assertTrue(CustomerManagement.getCustomers().containsValue(customer));
+        assertEquals(CustomerManagement.getCustomer(customer.getId()), customer);
     }
 }

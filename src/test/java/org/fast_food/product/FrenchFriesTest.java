3fb87bdb1edeadb44e7fa7c1c22333637d2e4b70
Bartosz <bertolem557@gmail.com>
2024-02-07 16:51:14 +0100

    add price for FrenchFries

---
+++ src/test/java/org/fast_food/product/FrenchFriesTest.java
@@ -0,0 +1,33 @@
+package org.fast_food.product;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FrenchFriesTest {
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getCalories() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+}

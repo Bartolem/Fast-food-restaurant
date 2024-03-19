87129de77c3934076d3127b3e5d6127c9ccf5917
Bartosz <bertolem557@gmail.com>
2024-03-19 14:36:02 +0100

    delete CustomerCreation class

--- src/main/java/org/fast_food/customer/CustomerCreation.java
+++
@@ -1,9 +0,0 @@
-package org.fast_food.customer;
-
-public class CustomerCreation {
-    public static Customer createCustomerAccount(String firstName, String lastName, String email, String phoneNumber) {
-        Customer customer = new Customer(firstName, lastName, email, phoneNumber);
-        CustomerManagement.addCustomer(customer.getId(), customer);
-        return customer;
-    }
-}

772f5182b5048190edec7aa6f78066abf41bf7ba
Bartolem <bartolem557@gmail.com>
2024-04-17 17:21:01 +0200

    implement delete method in OrderDAOImpl

--- src/main/java/org/fast_food/database_connection/OrderDAOImpl.java
+++ src/main/java/org/fast_food/database_connection/OrderDAOImpl.java
@@ -15,6 +15,7 @@ import org.fast_food.product.ProductKeyDeserializer;
 
 import java.math.BigDecimal;
 import java.sql.*;
+import java.sql.Date;
 import java.util.*;
 
 public class OrderDAOImpl implements OrderDAO {
@@ -66,7 +67,46 @@ public class OrderDAOImpl implements OrderDAO {
 
     @Override
     public List<Order> getAll() throws SQLException {
-        return null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DatabaseConnector.connect();
+            preparedStatement = connection.prepareStatement("SELECT id, status, creation_date, total_price, customer_id, content, total_price_after_discount, discount FROM orders");
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                UUID orderId = (UUID) resultSet.getObject("id");
+                OrderStatus status = OrderStatus.valueOf(resultSet.getString("status"));
+                Timestamp creationDate = (Timestamp) resultSet.getObject("creation_date");
+                BigDecimal totalPrice = resultSet.getBigDecimal("total_price");
+                Customer customer = CustomerManagement.getCustomer((UUID) resultSet.getObject("customer_id"));
+                String jsonbString = resultSet.getString("content");
+
+                ObjectMapper mapper = new ObjectMapper();
+                SimpleModule module = new SimpleModule();
+                module.addKeyDeserializer(Product.class, new ProductKeyDeserializer());
+                mapper.registerModule(module);
+                HashMap<Product, Integer> content = mapper.readValue(jsonbString, new TypeReference<HashMap<Product, Integer>>(){});
+                BigDecimal totalPriceAfterDiscount = resultSet.getBigDecimal("total_price_after_discount");
+                BigDecimal discount = resultSet.getBigDecimal("discount");
+
+                Order order = new Order(orderId, status, creationDate.toLocalDateTime(), content, totalPrice, totalPriceAfterDiscount, discount, customer);
+                OrderManagement.addOrder(order);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } finally {
+            assert resultSet != null;
+            DatabaseConnector.closeResultSet(resultSet);
+            DatabaseConnector.closePreparedStatement(preparedStatement);
+            DatabaseConnector.closeConnection(connection);
+        }
+
+        return OrderManagement.getOrders().stream().toList();
     }
 
     @Override
